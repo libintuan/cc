1d0158354c8dce02073a2181a6017c48cdc3c082
tuantuan <xinnian2012a@163.com>
2021-03-23 14:52:15 +0800

    grass init

---
+++ src/main/java/com/sunset/grass/GrassApplication.java
@@ -0,0 +1,13 @@
+package com.sunset.grass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunset/grass/GrassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunset.grass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
